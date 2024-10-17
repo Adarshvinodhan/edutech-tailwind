c82fa04ab995a9a585e1e03b22787092a9137ff4
Adarsh <adarshvinodhan@gmail.com>
2024-10-17 23:30:34 +0530

    white space removed

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 document.getElementById('hamburger').onclick = function() {
-        const mobileMenu = document.getElementById('mobileMenu');
-        // Toggle visibility
-        if (mobileMenu.style.display === 'block') {
-            mobileMenu.style.display = 'none';
-        } else {
-            mobileMenu.style.display = 'block';
-        }
-    };
-    
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
+    const mobileMenu = document.getElementById('mobileMenu');
+    // Toggle visibility
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+};
 
-  hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
